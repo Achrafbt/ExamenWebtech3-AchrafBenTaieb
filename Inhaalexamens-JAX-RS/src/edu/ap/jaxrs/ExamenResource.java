95b4ea14e8cb7ff350372a37cf98ef62a4e13d82
Achraf <achraf.bentaieb@student.ap.be>
2016-08-29 15:06:42 +0200

    Commit 2 Jax-rs
    Examens tonen toegevoegd
    examen toevoegen toegevoegd

--- Inhaalexamens-JAX-RS/src/edu/ap/jaxrs/ExamenResource.java
+++ Inhaalexamens-JAX-RS/src/edu/ap/jaxrs/ExamenResource.java
@@ -1,5 +1,104 @@
 package edu.ap.jaxrs;
 
-public class ExamenResource {
+import java.io.*;
 
+import javax.ws.rs.*;
+import javax.json.*;
+
+@Path("/examens")
+public class ExamenResource {
+	
+	static final String FILE = "/Users/Achraf/Desktop/Examen.json";
+
+	@GET
+	@Produces({"text/html"})
+	public String getExamensHTML() {
+		String htmlString = "<html><body>";
+		try {
+			JsonReader reader = Json.createReader(new StringReader(getExamensJSON()));
+			JsonObject rootObj = reader.readObject();
+			JsonArray array = rootObj.getJsonArray("examens");
+			
+			
+			for(int i = 0 ; i < array.size(); i++) {
+				JsonObject obj = array.getJsonObject(i);
+				htmlString += "<b>Naam : " + obj.getString("naam") + "</b><br>";
+				htmlString += "Examen : " + obj.getString("examen") + "<br>";
+				htmlString += "Reden : " + obj.getString("reden") + "<br>";
+				htmlString += "Datum : " + obj.getString("datum") + "<br>";
+				htmlString += "<br><br>";
+			}
+		}
+		catch(Exception ex) {
+			htmlString = "<html><body>" + ex.getMessage();
+		}
+		
+		return htmlString + "</body></html>";
+	}
+	
+	@GET
+	@Produces({"application/json"})
+	public String getExamensJSON() {
+		String jsonString = "";
+		try {
+			InputStream fis = new FileInputStream(FILE);
+	        JsonReader reader = Json.createReader(fis);
+	        JsonObject obj = reader.readObject();
+	        reader.close();
+	        fis.close();
+	        
+	        jsonString = obj.toString();
+		} 
+		catch (Exception ex) {
+			jsonString = ex.getMessage();
+		}
+		
+		return jsonString;
+	}
+	
+	@POST
+	@Consumes({"application/json"})
+	public String addExamen(String examenJSON) {
+		String returnCode = "";
+		try {
+			// read existing products
+			InputStream fis = new FileInputStream(FILE);
+	        JsonReader jsonReader1 = Json.createReader(fis);
+	        JsonObject jsonObject = jsonReader1.readObject();
+	        jsonReader1.close();
+	        fis.close();
+	        
+	        JsonReader jsonReader2 = Json.createReader(new StringReader(examenJSON));
+	        JsonObject newObject = jsonReader2.readObject();
+	        jsonReader2.close();
+	        
+	        JsonArray array = jsonObject.getJsonArray("examens");
+	        JsonArrayBuilder arrBuilder = Json.createArrayBuilder();
+	        
+	        for(int i = 0; i < array.size(); i++){
+	        	// add existing examens
+	        	JsonObject obj = array.getJsonObject(i);
+	        	arrBuilder.add(obj);
+	        }
+	        // add new product
+	        arrBuilder.add(newObject);
+	        
+	        // now wrap it in a JSON object
+	        JsonArray newArray = arrBuilder.build();
+	        JsonObjectBuilder builder = Json.createObjectBuilder();
+	        builder.add("examens", newArray);
+	        JsonObject newJSON = builder.build();
+
+	        // write to file
+	        OutputStream os = new FileOutputStream(FILE);
+	        JsonWriter writer = Json.createWriter(os);
+	        writer.writeObject(newJSON);
+	        writer.close();
+		} 
+		catch (Exception ex) {
+			returnCode = ex.getMessage();
+		}
+		
+		return returnCode;
+	}
 }
