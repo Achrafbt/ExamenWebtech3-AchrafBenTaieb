ca03e9cc35034323814d8ee712f937ddc5e614ea
Achraf <achraf.bentaieb@student.ap.be>
2016-08-29 17:00:55 +0200

    commit 5 django
    django model definition

--- Django/inhaalexamens/main/models.py
+++ Django/inhaalexamens/main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Examen(models.Model):
+	examen_name = models.CharField(max_length=50)
+	examen_reden = models.CharField(max_length=500)
+
+	def __unicode__(self):
+		return self.examen_name
--- Django/inhaalexamens/main/views.py
+++ Django/inhaalexamens/main/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.template import RequestContext, loader
+from django.views import generic
 
-# Create your views here.
+from .models import Examen
+
+class IndexView(generic.ListView):
+    template_name = 'main/index.html'
+    context_object_name = 'examen_list'
+
+    def get_queryset(self):
+        return Author.objects.order_by('examen_name')
+
+def index(request):
+	examen_list = Examen.objects.order_by('examen_name')
+
+	#return HttpResponse(output)
+	return render(request, 'main/index.html', {'examen_list': examen_list})
